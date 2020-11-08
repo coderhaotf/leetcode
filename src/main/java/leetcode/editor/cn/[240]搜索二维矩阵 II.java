fd24e1da5a272f45bb7659b88be870ae09ae147b
haotengfei <hao15914361974@gmail.com>
2020-11-08 11:23:19 +0800

    MaxSubArray; SearchMatrix

---
+++ src/main/java/leetcode/editor/cn/[240]搜索二维矩阵 II.java
@@ -0,0 +1,54 @@
+package leetcode.editor.cn;
+
+//编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+//
+//
+// 每行的元素从左到右升序排列。
+// 每列的元素从上到下升序排列。
+//
+//
+// 示例:
+//
+// 现有矩阵 matrix 如下：
+//
+// [
+//  [1,   4,  7, 11, 15],
+//  [2,   5,  8, 12, 19],
+//  [3,   6,  9, 16, 22],
+//  [10, 13, 14, 17, 24],
+//  [18, 21, 23, 26, 30]
+//]
+//
+//
+// 给定 target = 5，返回 true。
+//
+// 给定 target = 20，返回 false。
+// Related Topics 二分查找 分治算法
+// 👍 470 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) return false;
+        int w = matrix[0].length;
+        int h = matrix.length;
+        boolean res = false;
+
+        int i = h - 1;
+        int j = 0;
+        while(i >= 0 && j < w) {
+            if (target == matrix[i][j]) return true;
+
+            if (target < matrix[i][j]) {
+                i--;
+                if (i < 0) return false;
+            }
+            if (target > matrix[i][j]) {
+                j++;
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
